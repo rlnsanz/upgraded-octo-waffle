458344e6db5c632b8a2f5f648e1a2dd4a353fad2
Rolando Garcia <rogarcia@berkeley.edu>
2019-10-10 18:07:49 -0700

    capture, any query

--- train.py
+++ train.py
@@ -30,49 +30,53 @@ from conf import settings
 from utils import get_network, get_training_dataloader, get_test_dataloader, WarmUpLR
 
 def train(epoch):
+    """
+    :globals: net, cifar100_training_loader, args, warmup_scheduler, torch
+              optimizer, loss_function
+    :param epoch:
+    :return:
+    """
 
     net.train()
-    for batch_index, (images, labels) in enumerate(cifar100_training_loader):
+    if not flor.SKIP:
+        for batch_index, (images, labels) in enumerate(cifar100_training_loader):
+            if epoch <= args.warm:
+                warmup_scheduler.step()
+
+            images = Variable(images)
+            labels = Variable(labels)
+
+            if torch.cuda.is_available():
+                labels = labels.cuda()
+                images = images.cuda()
+
+            optimizer.zero_grad()
+            outputs = net(images)
+            loss = loss_function(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            print('Training Epoch: {epoch} [{trained_samples}/{total_samples}]\tLoss: {:0.4f}\tLR: {:0.6f}'.format(
+                loss.item(),
+                optimizer.param_groups[0]['lr'],
+                epoch=epoch,
+                trained_samples=batch_index * args.b + len(images),
+                total_samples=len(cifar100_training_loader.dataset)
+            ))
+
+        # Store the globals
+        flor.store(net.state_dict())
         if epoch <= args.warm:
-            warmup_scheduler.step()
+            flor.store(warmup_scheduler.state_dict())
+        flor.store(optimizer.state_dict())
+    else:
+        net.load_state_dict(flor.load())
+        net.train()
+        if epoch <= args.warm:
+            warmup_scheduler.load_state_dict(flor.load)
+        optimizer.load_state_dict(flor.load())
+        loss = flor.load()
 
-        images = Variable(images)
-        labels = Variable(labels)
-
-        if torch.cuda.is_available():
-            labels = labels.cuda()
-            images = images.cuda()
-
-        optimizer.zero_grad()
-        outputs = net(images)
-        loss = loss_function(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        n_iter = (epoch - 1) * len(cifar100_training_loader) + batch_index + 1
-
-        last_layer = list(net.children())[-1]
-        # for name, para in last_layer.named_parameters():
-        #     if 'weight' in name:
-        #         writer.add_scalar('LastLayerGradients/grad_norm2_weights', para.grad.norm(), n_iter)
-        #     if 'bias' in name:
-        #         writer.add_scalar('LastLayerGradients/grad_norm2_bias', para.grad.norm(), n_iter)
-
-        print('Training Epoch: {epoch} [{trained_samples}/{total_samples}]\tLoss: {:0.4f}\tLR: {:0.6f}'.format(
-            loss.item(),
-            optimizer.param_groups[0]['lr'],
-            epoch=epoch,
-            trained_samples=batch_index * args.b + len(images),
-            total_samples=len(cifar100_training_loader.dataset)
-        ))
-
-        #update training loss for each iteration
-        # writer.add_scalar('Train/loss', loss.item(), n_iter)
-
-    # for name, param in net.named_parameters():
-    #     layer, attr = os.path.splitext(name)
-    #     attr = attr[1:]
-    #     writer.add_histogram("{}/{}".format(layer, attr), param, epoch)
 
 def eval_training(epoch):
     net.eval()
@@ -80,29 +84,32 @@ def eval_training(epoch):
     test_loss = 0.0 # cost function error
     correct = 0.0
 
-    for (images, labels) in cifar100_test_loader:
-        images = Variable(images)
-        labels = Variable(labels)
+    if not flor.SKIP:
+        for (images, labels) in cifar100_test_loader:
+            images = Variable(images)
+            labels = Variable(labels)
 
-        if torch.cuda.is_available():
-            images = images.cuda()
-            labels = labels.cuda()
+            if torch.cuda.is_available():
+                images = images.cuda()
+                labels = labels.cuda()
 
-        outputs = net(images)
-        loss = loss_function(outputs, labels)
-        test_loss += loss.item()
-        _, preds = outputs.max(1)
-        correct += preds.eq(labels).sum()
+            outputs = net(images)
+            loss = loss_function(outputs, labels)
+            test_loss += loss.item()
+            _, preds = outputs.max(1)
+            correct += preds.eq(labels).sum()
+        flor.store(test_loss)
+        flor.store(correct)
+    else:
+        test_loss = flor.load()
+        correct = flor.load()
+
 
     print('Test set: Average loss: {:.4f}, Accuracy: {:.4f}'.format(
         test_loss / len(cifar100_test_loader.dataset),
         correct.float() / len(cifar100_test_loader.dataset)
     ))
     print()
-
-    #add informations to tensorboard
-    # writer.add_scalar('Test/Average loss', test_loss / len(cifar100_test_loader.dataset), epoch)
-    # writer.add_scalar('Test/Accuracy', correct.float() / len(cifar100_test_loader.dataset), epoch)
 
     return correct.float() / len(cifar100_test_loader.dataset)
 
@@ -144,40 +151,11 @@ if __name__ == '__main__':
     warmup_scheduler = WarmUpLR(optimizer, iter_per_epoch * args.warm)
     checkpoint_path = os.path.join(settings.CHECKPOINT_PATH, args.net, settings.TIME_NOW)
 
-    #use tensorboard
-    # if not os.path.exists(settings.LOG_DIR):
-    #     os.mkdir(settings.LOG_DIR)
-    # writer = SummaryWriter(log_dir=os.path.join(
-    #         settings.LOG_DIR, args.net, settings.TIME_NOW))
-    # input_tensor = torch.Tensor(12, 3, 32, 32).cuda()
-    # writer.add_graph(net, Variable(input_tensor, requires_grad=True))
-
-    #create checkpoint folder to save model
-    if not os.path.exists(checkpoint_path):
-        os.makedirs(checkpoint_path)
-    checkpoint_path = os.path.join(checkpoint_path, '{net}-{epoch}-{type}.pth')
-
     best_acc = 0.0
     for epoch in range(1, settings.EPOCH):
 
         if epoch > args.warm:
             train_scheduler.step(epoch)
 
-        if not flor.SKIP:
-            train(epoch)
-            flor.store(net.state_dict())
-        else:
-            net.load_state_dict(flor.load())
-
+        train(epoch)
         acc = eval_training(epoch)
-
-        #start to save best performance model after learning rate decay to 0.01 
-        # if epoch > settings.MILESTONES[1] and best_acc < acc:
-        #     torch.save(net.state_dict(), checkpoint_path.format(net=args.net, epoch=epoch, type='best'))
-        #     best_acc = acc
-        #     continue
-
-        # if not epoch % settings.SAVE_EPOCH:
-        #     torch.save(net.state_dict(), checkpoint_path.format(net=args.net, epoch=epoch, type='regular'))
-
-    # writer.close()
