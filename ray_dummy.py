7b224071ae39459056eff9aa455e1e16877de0e9
Rolando Garcia <rogarcia@berkeley.edu>
2019-12-04 09:57:49 -0800

    updating dummy

--- ray_dummy.py
+++ ray_dummy.py
@@ -10,6 +10,11 @@ def do_epoch_dummy(partition):
     for epoch in partition:
         print(epoch)
 
+@ray.remote(num_gpus=1)
+def use_gpu():
+    print("ray.get_gpu_ids(): {}".format(ray.get_gpu_ids()))
+    print("CUDA_VISIBLE_DEVICES: {}".format(os.environ["CUDA_VISIBLE_DEVICES"]))
+
 epoch = 0
 
 import math
@@ -18,10 +23,13 @@ NUM_GPU = 8
 work_per_gpu = math.ceil(len(iterator) / NUM_GPU)
 i = 0
 partitions = []
+
 while i * work_per_gpu < len(iterator):
     partitions.append(iterator[i*work_per_gpu: (i+1)*work_per_gpu])
     i += 1
 
 futures = [do_epoch_dummy.remote(p) for p in partitions]
-ray.get(futures)
+gutures = [use_gpu.remote() for p in partitions]
 
+ray.get(futures)
+ray.get(gutures)
